040be3b9eededee0181ec40208d4fc98dc6b74d4
Giga <gigalomidze2010@gmail.com>
2025-07-15 23:13:51 +0400

    1

---
+++ level 05/cw/cw.py
@@ -0,0 +1,11 @@
+num1 = int(input("შეიყვანეთ თქვენი პირველი რიცხვი"))
+num2 = int(input("შეიყვანეთ თქვენი მეორე რიცხვი"))
+
+operation1 = num1 > num2
+print(operation1)
+
+operation2 = num1 < num2
+print(operation2)
+
+operation3 = num1 == num2
+print(operation3)
