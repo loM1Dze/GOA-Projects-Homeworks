947371ac776c3c9b2ca17126f76369ad1e70d166
Giga <gigalomidze2010@gmail.com>
2025-09-13 22:50:24 +0400

    1

---
+++ level 22/cw/cw.py
@@ -0,0 +1,26 @@
+# 1) შექმენით სია სადაც გექნებათ მოთავსებული მხოლოდ და მხოლოდ სტრინგ ტიპის მონაცემები(სიაში შეიყვანეთ 10 სახელი),თქვენი დავალებაა ამ სიიდან გამოიტანოთ თითეული სახელი ოღნდ ცალ ცალკე,გამოიოყენეთ ინდექსინგი
+
+manqanebi = [
+    "BMW",
+    "Mercedes-Benz",
+    "Audi",
+    "Toyota",
+    "Honda",
+    "Ford",
+    "Chevrolet",
+    "Nissan",
+    "Hyundai",
+    "Kia"
+]
+
+# ინდექსირებით თითოეულის გამოტანა
+print(manqanebi[0])
+print(manqanebi[1])
+print(manqanebi[2])
+print(manqanebi[3])
+print(manqanebi[4])
+print(manqanebi[5])
+print(manqanebi[6])
+print(manqanebi[7])
+print(manqanebi[8])
+print(manqanebi[9])
