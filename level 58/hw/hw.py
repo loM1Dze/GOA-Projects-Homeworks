73c8d38a28163bbacab032a9c71a0b688d88a35c
Giga <gigalomidze2010@gmail.com>
2026-01-25 19:51:31 +0400

    1

--- level 58/hw/hw.py
+++ level 58/hw/hw.py
@@ -35,3 +35,45 @@ Palindrom = ""
 for i in name:
     Palindrom = i + Palindrom
 print(name == Palindrom)
+
+
+# 4) შექმენით არეული რიცხვებით სავსე გრძელი სია და 2 ცარიელი სია, ერთ სიაში ჩააგდეთ ყველა ის რიცხვი რომელიც არის ლუწი და დგას 
+# კენტ ინდექსზე, ხოლო მეორე სიაში ჩააგდეთ ყველა ის რიცხვი რომელიც არის ლუწი და დგას კენტ ინდექსზე, გამოიყენეთ for ციკლი.
+
+nums = [5, 8, 3, 12, 7, 6, 9, 10, 4, 11]
+nums1 = []
+nums2 = []
+
+for i in range(len(nums)):
+    if i % 2 == 1:
+        if nums[i] % 2 == 0:
+            nums1.append(nums[i])
+        else:
+            nums2.append(nums[i])
+
+print(nums1)
+print(nums2)
+
+
+# 5) შექმენით ყველანაირი მონაცემთა ტიპების ელემენტებით სავსე სია, ამოშალეთ ყველა დუპლიკატები - ყველაფერი რაც მეორდება 2-ზე მეტჯერ, გამოიყენეთ remove() ფუნქცია და while ციკლი.
+
+data = [1, 2, 2, 3, "a", "a", "b", True, True, False, 1, 1, 1]
+
+i = 0
+while i < len(data):
+    count = 0
+    j = 0
+
+    
+    while j < len(data):
+        if data[i] == data[j]:
+            count += 1
+        j += 1
+
+    
+    if count > 1:
+        data.remove(data[i])
+    else:
+        i += 1
+
+print(data)
