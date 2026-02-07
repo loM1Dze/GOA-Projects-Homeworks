b74be5ca71bc52a4452d7fe6da23f3f1dc8c4a6e
Giga <gigalomidze2010@gmail.com>
2026-02-07 23:45:50 +0400

    1

--- level 64/cw/cw.py
+++ level 64/cw/cw.py
@@ -12,3 +12,23 @@ def say_my_name():
 say_my_name()
 say_my_name()
 say_my_name()
+
+
+# 2)შექმენი ფუნქცია სახელად check_even()
+
+# ფუნქციის შიგნით შექმენი ცვლადი სადაც შიინახავ რაიმე რიცხვს
+
+# შემდეგ შეამოწმე-->
+
+# თუ ცვლადში შენახული რიცხვი არის ლუწი --- ფუნქციამ დააბრუნოს ==> "this number is even"
+
+# სხვა შემთხვევაში --- ფუნქციამდ ააბრუნოს ==> "this number is odd"
+
+def check_even():
+    num = 4
+    if num % 2 == 0:
+        return "this number is even"
+    else:
+        return "this number is odd"
+
+print(check_even())
