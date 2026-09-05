0e188df69eda3de265adacad488874619a63e330
F <abywss1@gmail.com>
2026-09-05 14:50:41 +0400

    1

---
+++ level 124/cw/cw.js
@@ -0,0 +1,33 @@
+// 1)შენი დავალებაა რომ გამოიტანო შენი სახელი გვარი დიდ ასოებში , შემდეგ გამოიტანე შენი მისამართი პატარა ასოები
+
+console.log('giga lomidze'.toUpperCase(), 'GUDAMAKHARI'.toLowerCase())
+
+
+// 2)შენი დავალებაა რომ შენს სახელს რომელსაც გვერდებიდან აქვს white spaces მოაშორო და დააკონსოლო იგი სფეისების გარეშე
+
+console.log('            giga           '.trim())
+
+
+// 3)რომელიმე ორ რიცხვზე მოახდინე ყველა მათემატიკური ოპერაცია რაც ვისწავლეთ
+
+console.log(200 + 2)
+console.log(200 - 2)
+console.log(200 / 2)
+console.log(200 * 2)
+console.log(200 % 2)
+
+
+// 4)შენი დავალებაა რომ --> "i live in Tbilisi" სიტყვა live ჩაანაცვლო სიტყვა "love" ით და გამოიტანო განახლებული სტრინგი
+
+console.log('i live in Tbilisi'.replace('live', 'love'))
+
+
+// 5))შენი დავალებაა რომ --> "i live in Tbilisi and also i live in Georgia" ყველა სიტყვა live ჩაანაცვლო სიტყვა "love" ით და გამოიტანო განახლებული სტრინგი
+
+
+console.log('i live in Tbilisi and also i live in Georgia'.replaceAll('live', 'love'))
+
+
+// 6)გამოიტანე შენი მისამართი 10 ჯერ კონსოლში
+
+console.log('gudamakhari'.repeat(10))
