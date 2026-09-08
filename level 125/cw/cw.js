30b69c41097f08b545e87ccdc8b8c1c0a9dfcee5
F <abywss1@gmail.com>
2026-09-08 14:57:07 +0400

    1

---
+++ level 125/cw/cw.js
@@ -0,0 +1,53 @@
+let x = 20;
+
+x += 10
+console.log(x); // 30
+
+x -= 6
+console.log(x); // 24
+
+x *= 2
+console.log(x); // 48
+
+x /= 4
+console.log(x); // 12
+
+x **= 3
+console.log(x); // 1728
+
+x %= 5
+console.log(x); // 3
+
+x ++
+console.log(x) // 21
+
+x --
+console.log(x) // 19
+
+
+
+
+
+
+
+const name = 'გიორგი'
+const lastName = 'ბერიძე'
+const age = 22
+const hobby = 'ფეხბურთის თამაში'
+
+const sentence = `გამარჯობა, მე ვარ ${name}${lastName}, ვარ ${age} წლის და ჩემი ჰობია ${hobby}.`
+
+
+console.log(sentence)
+
+////////////////////////////////////////////////////////
+
+
+const txt = 'გამარჯობა'
+const idk= true
+const num = 25
+
+
+console.log(typeof txt)
+console.log(typeof idk)
+console.log(typeof num)
