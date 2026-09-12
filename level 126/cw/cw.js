2a48ce2afc61bfe8e842c382e5a39dc7cb35bef8
F <abywss1@gmail.com>
2026-09-12 14:59:27 +0400

    1

---
+++ level 126/cw/cw.js
@@ -0,0 +1,23 @@
+// let firstName = prompt("შეიყვანეთ თქვენი სახელი:")
+// let lastName = prompt("შეიყვანეთ თქვენი გვარი:")
+// let age = prompt("შეიყვანეთ თქვენი ასაკი:")
+// let address = prompt("შეიყვანეთ თქვენი მისამართი:")
+
+// let sentence = `გამარჯობა, მე ვარ ${firstName} ${lastName}, ${age} წლის და ვცხოვრობ მისამართზე: ${address}.`
+
+// console.log(sentence)
+
+
+
+
+
+
+let number = 15;
+
+if (number > 0) {
+  console.log("positive")
+} else if (number < 0) {
+  console.log("negative")
+} else {
+  console.log("zero")
+}
